6953b89972149b127ce81242e45b5523dfd8cf5e
AP <predoiu.seli@gmail.com>
2016-01-13 05:37:05 -0800

    add util fct

---
+++ fct/fct.sh
@@ -0,0 +1,10 @@
+function vdo() {
+    docker run -it --rm -p 1521:1521 $(docker images | awk '{print $1}' | grep oracle ) /bin/bash
+}
+function vdip() {
+    docker inspect $(docker ps -qa) | grep \"IPAddress\" | sed 's/,//g' | awk  '{print $2}' | sed 's/\"//g'
+}
+
+function vdrm() {
+    docker rm $(docker ps --all -qa)
+}
