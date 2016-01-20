912aba9e7bc4bbd2810ee0ca3fc06dd43e630b4c
AP <predoiu.seli@gmail.com>
2016-01-20 05:23:26 -0800

    add volume to docker

--- fct/fct.sh
+++ fct/fct.sh
@@ -1,5 +1,5 @@
 function vdo() {
-    docker run -it --rm -p 1521:1521 $(docker images | awk '{print $1}' | grep oracle ) /bin/bash
+    docker run -it --rm -p 1521:1521 -v ~/test/alim:/alim $(docker images | awk '{print $1}' | grep oracle ) /bin/bash
 }
 function vdip() {
     docker inspect $(docker ps -qa) | grep \"IPAddress\" | sed 's/,//g' | awk  '{print $2}' | sed 's/\"//g'
